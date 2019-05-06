0614648894af6db95fe182f2f6a31aafc8d50a63
MrShafqatNadeem <mistershafqatnadeem@gmail.com>
2019-05-06 20:01:29 +0500

    Second Update

--- app/src/main/java/com/example/prsystem/MainActivity.java
+++ app/src/main/java/com/example/prsystem/MainActivity.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView goto_sign_up;
+    Button login_button;
+    Spinner sign_in_as_spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +26,36 @@ public class MainActivity extends AppCompatActivity {
         goto_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goto_sign_up_activity  = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent goto_sign_up_activity = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(goto_sign_up_activity);
             }
         });
+
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goto_search = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(goto_search);
+            }
+        });
+
+    sign_in_as_spinner = findViewById(R.id.sign_as);
+        ArrayAdapter<CharSequence> adapter = (ArrayAdapter<CharSequence>) ArrayAdapter.createFromResource(this, R.array.sign_in_as,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        sign_in_as_spinner.setAdapter(adapter);
+        sign_in_as_spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, "Signing In as "+text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
--- app/src/main/java/com/example/prsystem/SearchActivity.java
+++ app/src/main/java/com/example/prsystem/SearchActivity.java
@@ -1,13 +1,50 @@
 package com.example.prsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Button search_button;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        spinner = findViewById(R.id.spinner);
+        search_button = findViewById(R.id.searcch_button);
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goto_result = new Intent(SearchActivity.this, ResultActivity.class);
+                startActivity(goto_result);
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.search_as, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, "Person Will be searched in " + text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }

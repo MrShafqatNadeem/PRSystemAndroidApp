e6036bb2a812c1dc264a3ba8576a84ac58e36276
MrShafqatNadeem <mistershafqatnadeem@gmail.com>
2019-06-12 18:20:22 +0500

    Second Update

--- app/src/main/java/com/example/prsystem/SearchActivity.java
+++ app/src/main/java/com/example/prsystem/SearchActivity.java
@@ -1,50 +1,69 @@
 package com.example.prsystem;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SearchActivity extends AppCompatActivity {
+
     Button search_button;
-    Spinner spinner;
+    Button entry_Button;
+    WebView data_view;
+    ImageView topimageicon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        spinner = findViewById(R.id.spinner);
+
+        topimageicon = findViewById(R.id.userImage);
+        final LinearLayout search_layout = findViewById(R.id.search_layout);
+        data_view = findViewById(R.id.data_view);
+        entry_Button = findViewById(R.id.entry_button);
         search_button = findViewById(R.id.searcch_button);
 
+        entry_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search_layout.setVisibility(View.GONE);
+                topimageicon.setVisibility(View.GONE);
+                //data_view.setWebViewClient(new WebViewClient());
+                data_view.setVisibility(View.VISIBLE);
+
+                String loadurl = "http://192.168.137.1:5555/entry";
+                data_view.getSettings().setJavaScriptEnabled(true);
+                data_view.setWebViewClient(new WebViewClient());
+                data_view.loadUrl(loadurl);
+
+
+
+
+            }
+        });
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goto_result = new Intent(SearchActivity.this, ResultActivity.class);
-                startActivity(goto_result);
+                search_layout.setVisibility(View.GONE);
+                topimageicon.setVisibility(View.GONE);
+                data_view.setVisibility(View.VISIBLE);
+
+                String loadurl = "http://192.168.137.1:5555/search";
+                data_view.getSettings().setJavaScriptEnabled(true);
+                data_view.setWebViewClient(new WebViewClient());
+
+                data_view.loadUrl(loadurl);
+
+
+
+
             }
         });
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.search_as, R.layout.support_simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(this, "Person Will be searched in " + text, Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
